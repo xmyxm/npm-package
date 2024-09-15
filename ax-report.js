72170b32686ae7bfc16e007c073b76764e0aa744
chenxuanfeng <chenxuanfeng@meituan.com>
2024-09-15 15:33:11 +0800

    feat: up

--- ax-report.js
+++ ax-report.js
@@ -1,6 +1,7 @@
 
 try {
     const axInfo = require('@dp/acinonyx-info');
+    // 上报依赖安装次数
     axInfo.report({
         material:{
             name:"@dp/fish",
@@ -10,4 +11,3 @@ try {
 } catch (error) {
    console.log('>>>>信息收集失败！')         
 };
-        
